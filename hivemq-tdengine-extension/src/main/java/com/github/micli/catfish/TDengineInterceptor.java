57fb596b97ca299a4982d801a3982f26c22f2c3a
Michael Li <mic_lee2000@hotmail.com>
2020-10-03 10:04:42 +0800

    Update copyright

--- hivemq-tdengine-extension/src/main/java/com/github/micli/catfish/TDengineInterceptor.java
+++ hivemq-tdengine-extension/src/main/java/com/github/micli/catfish/TDengineInterceptor.java
@@ -1,19 +1,27 @@
 
 /*
- * Copyright 2018-present HiveMQ GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* MIT License
+* 
+* Copyright (c) 2020 Michael Li
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
 package com.github.micli.catfish;
 
@@ -25,16 +33,15 @@ import com.hivemq.extension.sdk.api.interceptor.publish.parameter.PublishInbound
 import com.hivemq.extension.sdk.api.packets.publish.ModifiablePublishPacket;
 
 import java.util.Base64;
-import java.util.Optional;
 import java.nio.ByteBuffer;
 
 /**
- * This is a very simple {@link PublishInboundInterceptor}, it changes the
- * payload of every incoming PUBLISH with the topic 'hello/world' to 'Hello
- * World!'.
+ * This is a very simple {@link PublishInboundInterceptor}, 
+ * it retrieve every mqtt message 
+ * and save data to TDengine by REST API.
  *
- * @author Yannick Weber
- * @since 4.3.1
+ * @author Michael Li
+ * @since 0.1 
  */
 public class TDengineInterceptor implements PublishInboundInterceptor {
 
@@ -45,14 +52,17 @@ public class TDengineInterceptor implements PublishInboundInterceptor {
             final @NotNull PublishInboundOutput publishInboundOutput) {
         final ModifiablePublishPacket publishPacket = publishInboundOutput.getPublishPacket();
         final ClientInformation clientInformation = publishInboundInput.getClientInformation();
-
+        if(!publishPacket.getPayload().isPresent())
+                return;
+        
         try {
             String msgid = String.valueOf(publishPacket.getPacketId());
             String topic = publishPacket.getTopic();
             String deviceId = clientInformation.getClientId();
-            Optional<ByteBuffer> buff = publishPacket.getPayload();
+            ByteBuffer buff = publishPacket.getPayload().get();
             String payLoad = "";
-            byte[] bytes = buff.get().array().clone();
+            byte[] bytes = new byte[buff.remaining()];
+            buff.get(bytes);
             payLoad = encoder.encodeToString(bytes);
             // String payLoad = encoder.encodeToString(buff.get().array().clone());
             int qos = publishPacket.getQos().getQosNumber();
